b71d9bd25d25e44e0f0cfbe9b0f0d6cbe39879c8
mschnure <mschnur3@jhmi.edu>
2024-06-27 10:52:53 -0400

    pos-processing small edits

--- results/postProcessing.R
+++ results/postProcessing.R
@@ -8,7 +8,7 @@ INT.START.YEAR=2023
 INT.END.YEAR=2030
 END.YEAR=2040 
 
-SCENARIOS = c(1:7)
+SCENARIOS = 1 #c(1:8)
 HIV.SCENARIOS = c("noint", # NCD scen 1
                   "noint", # NCD scen 2
                   "retsupp",  # NCD scen 3
@@ -17,19 +17,21 @@ HIV.SCENARIOS = c("noint", # NCD scen 1
                   "tsteng",  # NCD scen 6
                   "comp"  # NCD scen 7
                      )
-REPLICATIONS = c(1:2)
-n.reps=2
+REPLICATIONS = 1 #c(1:2)
+n.reps=1
 OUTPUTS.DIR = "outputs/"
 
 ncd.simset=read.ncd.simset()
-save(ncd.simset, file = paste0("outputs/ncd.simset_",Sys.Date(),".Rdata"))
+#save(ncd.simset, file = paste0("outputs/ncd.simset_",Sys.Date(),".Rdata"))
 # khm.simset=read.khm.simset()
 khm.simset.full = read.khm.simset.full()
 
 results.array.cumulative = generate.cumulative.events.results.array(ncd.simset,n.reps=n.reps)
-save(results.array.cumulative, file = paste0("outputs/ncd.results.array.cumulative_",Sys.Date(),".Rdata"))
+#save(results.array.cumulative, file = paste0("outputs/ncd.results.array.cumulative_",Sys.Date(),".Rdata"))
 
 results.array.annual = generate.annual.events.results.array(ncd.simset,n.reps=n.reps)
-save(results.array.annual, file = paste0("outputs/ncd.results.array.annual_",Sys.Date(),".Rdata"))
+#save(results.array.annual, file = paste0("outputs/ncd.results.array.annual_",Sys.Date(),".Rdata"))
 
 
+# Checking annual NCD treatment coverage (to calibrate at baseline)
+ncd.simset[[1]][[1]]$annual.ncd.trt.coverage
